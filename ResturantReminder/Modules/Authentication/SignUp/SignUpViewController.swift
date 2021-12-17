33c0b825c7d204f5b68a09b6f10d2bcf48d344bc
FarazAhmedKhan <farazahmedkhan18@gmail.com>
2021-12-18 04:47:31 +0500

    [UI Fixes]

--- ResturantReminder/Modules/Authentication/SignUp/SignUpViewController.swift
+++ ResturantReminder/Modules/Authentication/SignUp/SignUpViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 class SignUpViewController: UIViewController {
 
     // MARK: - OUTLETS
+    @IBOutlet weak var textFieldFullName: UITextField!
+    @IBOutlet weak var textFieldPostalCode: UITextField!
+    @IBOutlet weak var textFieldEmail: UITextField!
+    @IBOutlet weak var textFieldPassword: UITextField!
+    @IBOutlet weak var textFieldConfirmPassword: UITextField!
     
     // MARK: - VARIABLES
     
@@ -17,10 +22,13 @@ class SignUpViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.connectFields()
     }
     
     // MARK: - SETUP VIEW
-    
+    private func connectFields() {
+        UITextField.connectFields(fields: [self.textFieldFullName, self.textFieldPostalCode, self.textFieldEmail, self.textFieldPassword, self.textFieldConfirmPassword])
+    }
     
     // MARK: - BUTTON ACTIONS
     @IBAction func didTapSignUpButton(_ sender: UIButton) {
